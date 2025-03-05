ba22d38204a58e8072e0d2a479e555f8cc4f5b19
Anas Alamin <anasalamin1997@gmail.com>
2025-03-05 21:33:14 +0100

    Revert "fix: overwritten title in pages"
    This reverts commit a11626bdaacba9a68f45c764edb930f1982611e7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import './globals.css'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata: Metadata = {
+  title: 'Aampere - Electric Vehicle Marketplace',
+  description: 'Find and compare the best electric vehicles on the market.'
+}
 
 export default function RootLayout({
   children
